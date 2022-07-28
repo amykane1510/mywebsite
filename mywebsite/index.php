4c6a535ec6bfcae87a1e7ba55168764cc10e0ece
amykane1510 <110104607+amykane1510@users.noreply.github.com>
2022-07-28 19:00:08 +0000

    Add files via upload

---
+++ mywebsite/index.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html>
+	
+	<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<title>mywebsite</title>
+			<!-- CSS only -->
+			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" 
+			integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+	</head>
+			
+			<body>
+			<!-- header-->
+			<div class="header">
+			<header>
+					<nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+						<!-- Navbar content -->
+						 <a class="navbar-brand" href="#">Home</a>
+						 <a class="navbar-brand" href="#">Contacts</a>
+						 <a class="navbar-brand" href="#">Service</a>
+						 <a class="navbar-brand" href="#">Presentation</a>
+							<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+							</button>
+							<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+							<div class="navbar-nav">
+							  <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+							  <a class="nav-link" href="#">Features</a>
+							  <a class="nav-link" href="#">Pricing</a>
+							  <a class="nav-link disabled">Disabled</a>
+							</div>
+							</div>
+					</nav>
+					</header>
+						<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+							  <ol class="carousel-indicators">
+								<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+								<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+								<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+							  </ol>
+							  <div class="carousel-inner">
+								<div class="carousel-item active">
+								  <img src="image3.jpg" class="d-block w-100" alt="...">
+								</div>
+								<div class="carousel-item">
+								  <img src="image1.jpg" class="d-block w-100" alt="...">
+								</div>
+								<div class="carousel-item">
+								  <img src="image2.jpg" class="d-block w-100" alt="...">
+								</div>
+							  </div>
+							  <button class="carousel-control-prev" type="button" data-target="#carouselExampleIndicators" data-slide="prev">
+								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+								<span class="sr-only">Previous</span>
+							  </button>
+							  <button class="carousel-control-next" type="button" data-target="#carouselExampleIndicators" data-slide="next">
+								<span class="carousel-control-next-icon" aria-hidden="true"></span>
+								<span class="sr-only">Next</span>
+							  </button>
+						</div>
+			
+			</div>
+			<h1 class="display-1" style="tex-align: center;">BIENVENUE	DANS LA NATURE</h1>
+			
+					<!-- section pour information  -->
+				<section>
+					<div class="row">
+						<div class="col-lg-6">
+							<img src="image4.jpg">
+							
+		
+						</div>
+						<div class="col-lg-6">
+						
+							What is Lorem Ipsum?
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+							when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
+							 into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum 
+							 passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+							Why do we use it?
+							It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem 
+							Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
+							 Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many 
+							 web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+
+                              </p>
+						</div>
+						<p>
+					</div>
+				</section>
+				
+				
+				
+								<section>
+					<div class="row">
+						<div class="col-lg-6">
+							<p>What is Lorem Ipsum?
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+							when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap 
+							into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with 
+							the release of Letraset sheets containing Lorem Ipsum passages, and more recently 
+							with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Why do we use it?
+							It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem 
+							Ipsum is that it has 
+							a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop 
+							publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites 
+							still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+
+							</p>
+		
+						</div>
+						<div class="col-lg-6">
+						<img src="image6.jpg">
+
+
+				</div>
+													
+				</div>
+				</section>
+											
+											
+													<!-- Footer -->
+							<footer class="page-footer font-small indigo" style="background-color:#65B7F4">
+
+							  <!-- Footer Links -->
+							  <div class="container text-center text-md-left">
+
+								<!-- Grid row -->
+								<div class="row">
+
+								  <!-- Grid column -->
+								  <div class="col-md-3 mx-auto">
+
+									<!-- Links -->
+									<h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+
+									<ul class="list-unstyled">
+									  <li>
+										<a href="#!">Very long link 1</a>
+									  </li>
+									  <li>
+										<a href="#!">Very long link 2</a>
+									  </li>
+									  <li>
+										<a href="#!">Very long link 3</a>
+									  </li>
+									  <li>
+										<a href="#!">Very long link 4</a>
+									  </li>
+									</ul>
+
+								  </div>
+								  <!-- Grid column -->
+
+								  <hr class="clearfix w-100 d-md-none">
+
+								  <!-- Grid column -->
+								  <div class="col-md-3 mx-auto">
+
+									<!-- Links -->
+									<h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+
+									<ul class="list-unstyled">
+									  <li>
+										<a href="#!">Link 1</a>
+									  </li>
+									  <li>
+										<a href="#!">Link 2</a>
+									  </li>
+									  <li>
+										<a href="#!">Link 3</a>
+									  </li>
+									  <li>
+										<a href="#!">Link 4</a>
+									  </li>
+									</ul>
+
+								  </div>
+								  <!-- Grid column -->
+
+								  <hr class="clearfix w-100 d-md-none">
+
+								  <!-- Grid column -->
+								  <div class="col-md-3 mx-auto">
+
+									<!-- Links -->
+									<h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+
+									<ul class="list-unstyled">
+									  <li>
+										<a href="#!">Link 1</a>
+									  </li>
+									  <li>
+										<a href="#!">Link 2</a>
+									  </li>
+									  <li>
+										<a href="#!">Link 3</a>
+									  </li>
+									  <li>
+										<a href="#!">Link 4</a>
+									  </li>
+									</ul>
+
+								  </div>
+								  <!-- Grid column -->
+
+								  <hr class="clearfix w-100 d-md-none">
+
+								  <!-- Grid column -->
+								  <div class="col-md-3 mx-auto">
+
+									<!-- Links -->
+									<h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+
+									<ul class="list-unstyled">
+									  <li>
+										<a href="#!">Link 1</a>
+									  </li>
+									  <li>
+										<a href="#!">Link 2</a>
+									  </li>
+									  <li>
+										<a href="#!">Link 3</a>
+									  </li>
+									  <li>
+										<a href="#!">Link 4</a>
+									  </li>
+									</ul>
+
+								  </div>
+								  <!-- Grid column -->
+
+								</div>
+								<!-- Grid row -->
+
+							  </div>
+							  <!-- Footer Links -->
+
+							  <!-- Copyright -->
+							  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+								<a href="/"> MDBootstrap.com</a>
+							  </div>
+							  <!-- Copyright -->
+
+							</footer>
+							<!-- Footer -->
+				</footer>
+				<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+				<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+				<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+				<!-- JavaScript Bundle with Popper -->
+				<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" 
+				integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+			</body>
+</html>
